7e4abf6cd5afcc79bedaa2f492deaa2711fb5596
IlhamLamp <ilhamutomo24@gmail.com>
2025-01-19 06:50:20 +0700

    test actions runner

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // DUMMY COMMIT
   app.enableCors({
     origin: process.env.APP_CLIENT,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
